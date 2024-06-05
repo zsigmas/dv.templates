d93f05877076a2beb2966919c0b3dd4ca026e0f0
Luis Moris Fernandez <luis.moris.fernandez@gmail.com>
2024-06-05 18:19:03 +0200

    fix setup.R

--- tests/testthat/setup.R
+++ tests/testthat/setup.R
@@ -1,8 +1,15 @@
-package_name <- "dv.templates"
 # validation (S)
-vdoc <- source(
+vdoc <- local({
+  #                      ##########
+  # package_name is used # INSIDE # the sourced file below
+  #                      ##########  
+  package_name <- read.dcf("../../DESCRIPTION")[, "Package"]
+
+  source(
   system.file("validation", "utils-validation.R", package = package_name, mustWork = TRUE),
   local = TRUE
 )[["value"]]
+
+})
 specs <- vdoc[["specs"]]
 #  validation (F)
